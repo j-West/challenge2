2fccaca4005ba840bb329ff62807aa6169c6b73e
j-west <westja89@gmail.com>
2016-11-30 08:25:08 -0600

    Changed format of if conditional

--- main.js
+++ main.js
@@ -15,12 +15,18 @@ for (var smallest = 3000; smallest > 0; smallest--) {
 }
 
 for (var smallest = 3000; smallest > 0; smallest--) {
-  if (smallest % 10 === 0 && smallest % 9 === 0 && smallest % 8 === 0 &&
-    smallest % 7 === 0 && smallest % 6 === 0 && smallest % 5 ===  0 &&
-    smallest % 4 === 0 && smallest % 3 === 0 && smallest % 2 === 0 &&
-    smallest % 1 === 0) {
+  if (smallest % 10 === 0 &&
+      smallest % 9 === 0 &&
+      smallest % 8 === 0 &&
+      smallest % 7 === 0 &&
+      smallest % 6 === 0 &&
+      smallest % 5 ===  0 &&
+      smallest % 4 === 0 &&
+      smallest % 3 === 0 &&
+      smallest % 2 === 0 &&
+      smallest % 1 === 0) {
 
-    console.log("The smallest number that can be divided by each of the numbers from 1 to 10 without any remainder is " + smallest);
+      console.log("The smallest number that can be divided by each of the numbers from 1 to 10 without any remainder is " + smallest);
                         }
 
 }
